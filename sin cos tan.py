af9c6c99e6ac83f0eca8db2522fd05b3f2132f1d
Sufi <sufiansonic@gmail.com>
2025-06-15 14:44:56 +0900

    gu

---
+++ sin cos tan.py
@@ -0,0 +1,21 @@
+import math
+
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+
+print()
+
+print(math.ceil(4.49))
+print(math.floor(4.89))
+print(math.factorial(6))
+
+x = float(input("Enter a number: "))
+
+sin = math.sin(x)
+cos = math.cos(x)
+tan = math.tan(x)
+
+print(f"The sine of {x} is {sin}")
+print(f"The cosine of {x} is {cos}")
+print(f"The tangent of {x} is {tan}")
